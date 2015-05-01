83497a34ba5ebe3bb75c291c71e70d32bbf0e8f2
Eric Drechsel <eric@pdxhub.org>
2015-05-01 16:35:04 -0700

    jspm init etc

---
+++ config.js
@@ -0,0 +1,28 @@
+System.config({
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ],
+    "blacklist": []
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "https://github.jspm.io/*.js",
+    "npm:*": "https://npm.jspm.io/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.1.13",
+    "babel-runtime": "npm:babel-runtime@5.1.13",
+    "core-js": "npm:core-js@0.8.4",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:core-js@0.8.4": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
